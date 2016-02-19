ee8e4765e9a078f82f1613207dee314137b77243
Alan R. Grant <alanrgrant@outlook.com>
2016-02-19 00:30:42 -0500

    Modify comments in GameManager

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,62 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;		
-using UnityEngine.UI;					//Allows us to use UI.
+using UnityEngine.UI;		
 	
 public class GameManager : MonoBehaviour
 {
-	public float levelStartDelay = 2f;						//Time to wait before starting level in seconds.
-	public float turnDelay = 0.1f;							//Delay between each Player turn.
-	public int playerHealth = 100;							//Starting value for Player health.
-	public static GameManager instance = null;				//Static instance of GameManager which allows it to be accessed by any other script.
-	[HideInInspector] public bool playersTurn = true;		//Boolean to check if it's player's turn.
+	public float levelStartDelay = 2f;			//Time to wait before starting level in seconds.
+	public float turnDelay = 0.1f;				//Delay between each Player turn.
+	public int playerHealth = 100;				//Starting value for Player health.
+	public static GameManager instance = null;		//Static instance of GameManager which allows it to be accessed by any other script.
+	[HideInInspector] public bool playersTurn = true;	//Boolean to check if it's player's turn.
 		
 		
-	private Text levelText;									//Text to display current level number.
-	private GameObject levelImage;							//Image to block out level as levels are being set up, background for levelText.
-	private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
-	private int level = 1;									//Current level number, expressed in game as "Day 1".
-	private List<Enemy> enemies;							//List of all Enemy units, used to issue them move commands.
-	private bool enemiesMoving;								//Boolean to check if enemies are moving.
-	private bool doingSetup = true;							//Boolean to check if we're setting up board, prevent Player movement during setup.
+	private Text levelText;					//Text to display current level number.
+	private GameObject levelImage;				//Image to block out level as levels are being set up, background for levelText.
+	private BoardManager boardScript;			//Store a reference to our BoardManager which will set up the level.
+	private int level = 1;					//Current level number, expressed in game as "Day 1".
+	private List<Enemy> enemies;				//List of all Enemy units, used to issue them move commands.
+	private bool enemiesMoving;				//Boolean to check if enemies are moving.
+	private bool doingSetup = true;				//Boolean to check if we're setting up board, prevent Player movement during setup.
 		
 		
 		
 	//Awake is always called before any Start functions
 	void Awake()
 	{
-		//Check if instance already exists
+		//Check if instance already exists. If not set it to this instance.
 		if (instance == null)
-				
-			//if not, set instance to this
 			instance = this;
 			
-		//If instance already exists and it's not this:
+		//If instance is not this then destroy this
 		else if (instance != this)
-				
-			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
 			Destroy(gameObject);	
 			
-		//Sets this to not be destroyed when reloading scene
+		//Don't destroy this when reloading scene
 		DontDestroyOnLoad(gameObject);
 			
-		//Create new list of Enemy objects.
 		enemies = new List<Enemy>();
 			
-		//Get a component reference BoardManager script
+		//Get a reference to BoardManager script
 		boardScript = GetComponent<BoardManager>();
 			
-		//Call the InitGame function to initialize the first level 
+		//Initialize first level 
 		InitGame();
 	}
 		
 	//This is called each time a scene is loaded.
 	void OnLevelWasLoaded(int index)
 	{
-		//Add one to our level number.
-		level++;
-		//Call InitGame to initialize our level.
-		InitGame();
+		level++;	//Add one to our level number.
+		InitGame();    	//Initialize our level.
 	}
 		
 	//Initializes the game for each level.
@@ -68,7 +61,7 @@ public class GameManager : MonoBehaviour
 		//Get a reference to LevelImage by finding it by name.
 		levelImage = GameObject.Find("LevelImage");
 			
-		//Get a reference to LevelText's text component by finding it by name and calling GetComponent.
+		//Get a reference to LevelText's text component 
 		levelText = GameObject.Find("LevelText").GetComponent<Text>();
 			
 		//Set the text of levelText to the string "Day" + level.
@@ -92,11 +85,8 @@ public class GameManager : MonoBehaviour
 	//Hides black image used between levels
 	void HideLevelImage()
 	{
-		//Disable the levelImage gameObject.
-		levelImage.SetActive(false);
-			
-		//Set doingSetup to false allowing player to move again.
-		doingSetup = false;
+		levelImage.SetActive(false);  	//Make game board viewable again.
+		doingSetup = false;		//Allow player to move again.
 	}
 		
 	//Update is called every frame.
@@ -112,10 +102,9 @@ public class GameManager : MonoBehaviour
 		StartCoroutine (MoveEnemies ());
 	}
 		
-	//Call this to add the passed in Enemy to the List of Enemy objects.
+	//Add Enemy to List of Enemy objects
 	public void AddEnemyToList(Enemy script)
 	{
-		//Add Enemy to List enemies.
 		enemies.Add(script);
 	}
 		
@@ -149,16 +138,15 @@ public class GameManager : MonoBehaviour
 			yield return new WaitForSeconds(turnDelay);
 		}
 			
-		//Loop through List of Enemy objects.
+		//Loop through List of Enemy objects and move them.
 		for (int i = 0; i < enemies.Count; i++)
 		{
-			//Call the MoveEnemy function of Enemy at index i in the enemies List.
 			enemies[i].MoveEnemy ();
 				
 			//Wait for Enemy's moveTime before moving next Enemy, 
 			yield return new WaitForSeconds(enemies[i].moveTime);
 		}
-		//Once Enemies are done moving, set playersTurn to true so player can move.
+		//Enemies are done moving, sow allow player to move again.
 		playersTurn = true;
 			
 		//Enemies are done moving, set enemiesMoving to false.
