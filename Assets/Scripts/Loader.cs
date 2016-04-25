4d9ac99e82acf45a34711ce40be5018c1866bbd4
Alan R. Grant <alrgrant@ius.edu>
2016-04-25 15:21:06 -0400

    Add more documentation to Loader.cs

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -2,12 +2,19 @@
 using System.Collections;
 /// <summary>
 /// This class acts as the inital loader to begin running the GameManager and SoundManager. Its sole purpose is
-/// to instantiate the instances of these two classes used in the game. 
+/// to instantiate the instances of these two classes used in the game. Inherits from MonoBehavior.
 /// </summary>
 public class Loader : MonoBehaviour 
 {
-	public GameObject gameManager;			//GameManager prefab to instantiate.
-	public GameObject soundManager;              //SoundManager prefab to instantiate.
+     /// <summary>
+     /// Stores an instance of the GameManager prefab.
+     /// </summary>
+	public GameObject gameManager;	
+     
+     /// <summary>
+     /// Stores an instance of the SoundManager prefab.
+     /// </summary>		
+	public GameObject soundManager;              
 
      /// <summary>
      /// This method is a general UnityEngine method used to initialize data before the game actually starts.
